9995f6ec67f89f9f6eb385fcabd4228031a0fb4e
Sanny Do <sanny.do94@gmail.com>
2020-08-27 01:32:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
  final_results = data.each_with_object({}) do |(key, value), final_array|
     #binding.pry
- values.each do |inner_key, names|
+  values.each do |inner_key, names|
       names.each do |name|
       #binding.pry
       if !final_array[name]
