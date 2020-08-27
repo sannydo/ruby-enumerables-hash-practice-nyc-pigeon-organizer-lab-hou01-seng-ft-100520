342be3cd54193e5f3881a18b65ebe7d1ff1b0f25
Sanny Do <sanny.do94@gmail.com>
2020-08-27 01:23:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,7 +12,7 @@ def nyc_pigeon_organizer(data)
     end 
     if !final_array[name][key] = []
     end
-    final_array[name][key].push
+    final_array[name][key].push(inner_key)
   end
   binding.pry
 end
